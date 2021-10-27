1b126a50d889f83b8fc6ad3e78257be856dca382
Truongezgg <truongezgg@gmail.com>
2021-10-28 01:25:59 +0700

    Add simple todo list core future: 1. Create todo 2. Delete todo 3. Update status todo 4. Filter todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { ChangeEvent, KeyboardEvent, useEffect, useState } from "react";
+import classNames from "classnames";
+import { ChangeEvent, KeyboardEvent, useState } from "react";
 import styles from "./styles.module.scss";
 
-const STATUS = {
-  ACTIVE: 1,
-  COMPLETED: 0,
-};
+enum STATUS {
+  OPEN = 0,
+  COMPLETED = 1,
+}
 
 function App() {
   const [todos, setTodos] = useState([
-    { id: 1, name: "Pay electric bill", status: STATUS.ACTIVE },
-    { id: 2, name: "Walk the dog", status: STATUS.COMPLETED },
+    { id: 1, name: 'Say: "Hello world!"', status: STATUS.OPEN },
+    { id: 2, name: "Have a nice day!", status: STATUS.COMPLETED },
   ]);
 
-  const getToggleAllStatus = () => {
-    return todos.every((item) => item.status === STATUS.ACTIVE);
+  const getStatus = () => {
+    return todos.every((item) => item.status === STATUS.COMPLETED);
   };
-  const [toggleAll, setToggleAll] = useState(getToggleAllStatus());
+  const [checkedAll, setCheckedAll] = useState(getStatus());
+  const [status, setStatus] = useState<STATUS | undefined>();
 
-  const handleChangeStatus = (e: ChangeEvent<HTMLInputElement>, id: number) => {
+  const handleCheckedItem = (e: ChangeEvent<HTMLInputElement>, id: number) => {
     const item = todos.find((el) => el.id === id);
     if (item) {
       item.status = Number(e.target.checked);
       setTodos([...todos]);
     }
-    setToggleAll(getToggleAllStatus());
+    setCheckedAll(getStatus());
   };
 
-  const handleChangeToggleAll = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleCheckedAll = (e: ChangeEvent<HTMLInputElement>) => {
     const isChecked = e.target.checked;
     setTodos(todos.map((item) => ({ ...item, status: Number(isChecked) })));
-    setToggleAll(isChecked);
+    setCheckedAll(isChecked);
   };
 
   const removeItem = (id: number) => () => {
@@ -39,12 +41,17 @@ function App() {
   const addItem = (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
       const name = e.currentTarget.value;
-      setTodos([
-        ...todos,
-        { id: Date.now(), name: name, status: STATUS.COMPLETED },
-      ]);
+      setTodos([...todos, { id: Date.now(), name: name, status: STATUS.OPEN }]);
       e.currentTarget.value = "";
     }
+  };
+
+  const getTodos = (status?: STATUS) => {
+    if (status === STATUS.OPEN)
+      return todos.filter((item) => item.status === STATUS.OPEN);
+    if (status === STATUS.COMPLETED)
+      return todos.filter((item) => item.status === STATUS.COMPLETED);
+    return todos;
   };
 
   return (
@@ -52,55 +59,96 @@ function App() {
       <div className={styles.TodoApp}>
         <header className={styles.Header}>Todos</header>
         <section className={styles.Main}>
-          <input
-            type="checkbox"
-            checked={toggleAll}
-            onChange={handleChangeToggleAll}
-          />
-          <input
-            type="text"
-            name="NewTodo"
-            id="NewTodo"
-            placeholder="What needs to be done?"
-            onKeyDown={(e) => addItem(e)}
-          />
+          <div className={styles.NewTodo}>
+            <div className="TextBoxWrapper">
+              <input
+                type="checkbox"
+                checked={checkedAll}
+                onChange={handleCheckedAll}
+              />
+            </div>
+            <input
+              type="text"
+              className={styles.NewTodoInput}
+              placeholder="What needs to be done?"
+              onKeyDown={(e) => addItem(e)}
+            />
+          </div>
 
           <ul className={styles.TodoList}>
-            {todos.map((item) => (
+            {getTodos(status).map((item) => (
               <li className={styles.ToDoItem} key={item.id}>
-                <div className={item.status ? styles.LineThrough : ""}>
-                  <input
-                    type="checkbox"
-                    name={String(item.id)}
-                    checked={Boolean(item.status)}
-                    onChange={(e) => {
-                      handleChangeStatus(e, item.id);
-                    }}
-                  />
-                  {item.name}
+                <div
+                  className={classNames(
+                    styles.ToDoItemContainer,
+                    item.status ? styles.LineThrough : ""
+                  )}
+                >
+                  <div className={styles.TextBoxWrapper}>
+                    <input
+                      type="checkbox"
+                      name={String(item.id)}
+                      checked={Boolean(item.status)}
+                      onChange={(e) => {
+                        handleCheckedItem(e, item.id);
+                      }}
+                    />
+                  </div>
+                  <div className={item.status ? styles.ToDoItemCompleted : ""}>
+                    {item.name}
+                  </div>
                 </div>
-                <button
+                <div
                   className={styles.DestroyItem}
                   onClick={removeItem(item.id)}
                 >
                   X
-                </button>
+                </div>
               </li>
             ))}
           </ul>
+          {!!todos.length && (
+            <footer className={styles.Footer}>
+              <div className={styles.TodoCount}>
+                {getTodos(STATUS.OPEN).length > 1
+                  ? getTodos(STATUS.OPEN).length + " items left"
+                  : getTodos(STATUS.OPEN).length + " item left"}
+              </div>
+              <ul className={styles.Filter}>
+                <li
+                  className={classNames(
+                    styles.FilterAll,
+                    status === undefined ? styles.Active : ""
+                  )}
+                  onClick={() => setStatus(undefined)}
+                >
+                  All
+                </li>
+                <li
+                  className={classNames(
+                    styles.FilterOPEN,
+                    status === STATUS.OPEN ? styles.Active : ""
+                  )}
+                  onClick={() => setStatus(STATUS.OPEN)}
+                >
+                  Open
+                </li>
+                <li
+                  className={classNames(
+                    styles.FilterCompleted,
+                    status === STATUS.COMPLETED ? styles.Active : ""
+                  )}
+                  onClick={() => setStatus(STATUS.COMPLETED)}
+                >
+                  Completed
+                </li>
+              </ul>
+            </footer>
+          )}
         </section>
-
-        <footer className={styles.Footer}>
-          <div className={styles.TodoCount}>Total: {todos.length}</div>
-          <ul className={styles.Filter}>
-            <li className={styles.FilterAll}>All</li>
-            <li className={styles.FilterActive}>Active</li>
-            <li className={styles.FilterCompleted}>Completed</li>
-          </ul>
-        </footer>
       </div>
 
-      <footer>Nguyen Duy Truong</footer>
+      <footer>Simple todo app - 2021/10/28</footer>
     </div>
   );
 }
